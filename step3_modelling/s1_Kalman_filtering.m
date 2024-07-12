b9fa9a9301f93172cdc14aaed8b362c8cc398644
Lingyu-Meng <147720003+Lingyu-Meng@users.noreply.github.com>
2024-07-12 03:24:37 +0100

    feat{step3}: fit model
    fit and compare multiple model.

--- step3_modelling/s1_Kalman_filtering.m
+++ step3_modelling/s1_Kalman_filtering.m
@@ -2,7 +2,7 @@
 % Estimate the value and uncertainty of each arm using Kalman filtering.
 % Calculate the V, RU, and VTU values for each trial.
 % V = Q(1) - Q(2); arm = 1 means left, arm = 2 means right
-% RU = σ(1) - σ(2); 
+% RU = σ(1) - σ(2);
 % VTU = sqrt(σ(1)^2 + σ(2)^2);
 
 workpath = pwd;
@@ -33,15 +33,15 @@ for n = 1:max(data.ID) % individual level
         if isempty(data_sub_block) % skip the block if there is no data
             continue;
         end
-
+        
         Q     = [0; 0]; % Q(1) is the value of the left arm, Q(2) is the value of the right arm
         sigma = [0; 0]; % sigma(1) is the uncertainty of the left arm, sigma(2) is the uncertainty of the right arm
         alpha = [0; 0]; % learning rate
         tau   = [0; 0]; % trial uncertainty
-
+        
         %Q(1) = data_sub_block.value_left;  % set prior value of arm 1
         %Q(2) = data_sub_block.value_right; % set prior value of arm 2
-% we can also consider the prior value as 4 for win and -4 for lose as it is the expectation of the whole task
+        % we can also consider the prior value as 4 for win and -4 for lose as it is the expectation of the whole task
         left_arm = string(data_sub_block.left(1));
         switch left_arm % set prior uncertainty of arm 1
             case '**S**'
@@ -57,7 +57,7 @@ for n = 1:max(data.ID) % individual level
                 alpha(1) = sigma(1)/(sigma(1) + tau_R);
                 tau(1) = tau_R;
         end
-
+        
         right_arm = string(data_sub_block.right(1));
         switch right_arm % set prior uncertainty of arm 2
             case '**S**'
@@ -73,27 +73,28 @@ for n = 1:max(data.ID) % individual level
                 alpha(2) = sigma(2)/(sigma(2) + tau_R);
                 tau(2) = tau_R;
         end
-
+        
         for j = 1:12 % trial level
             data_sub_block_trial = data_sub_block(data_sub_block.trial == j, :);
             if isempty(data_sub_block_trial) % skip the trial if there is no data
                 continue;
             end
             a_t = data_sub_block_trial.choice;
-                
-            Q(a_t)     = Q(a_t)     + alpha(a_t) * (data_sub_block_trial.payoff - Q(a_t));              
+            
+            Q(a_t)     = Q(a_t)     + alpha(a_t) * (data_sub_block_trial.payoff - Q(a_t));
             sigma(a_t) = sigma(a_t) - alpha(a_t) * sigma(a_t);
-
+            
             if sigma(a_t) == 0 % aviod NAN in safe arm learning
                 alpha(a_t) = 1;
             else
                 alpha(a_t) = sigma(a_t) / (sigma(a_t) + tau(a_t));
             end
-
-            data_sub_block_trial.V = Q(1) - Q(2);
+            
+            V = Q(1) - Q(2);
+            data_sub_block_trial.V = V;
             data_sub_block_trial.RU = sigma(1) - sigma(2);
-            data_sub_block_trial.VTU = sqrt(sigma(1)^2 + sigma(2)^2);
-
+            TU = sqrt(sigma(1)^2 + sigma(2)^2);
+            data_sub_block_trial.VTU = V / TU;
             data_sub_block(data_sub_block.trial == j, :) = data_sub_block_trial;
         end
         data_sub(data_sub.block_number == i, :) = data_sub_block;
@@ -102,4 +103,5 @@ for n = 1:max(data.ID) % individual level
 end
 
 cd("output/")
-save("estimated_data.csv","data")
+save("estimated_data.mat","data")
+cd(workpath)
