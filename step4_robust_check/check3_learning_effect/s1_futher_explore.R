53f9c4d5a5596226464a01f680f7c60f6ca1ad35
Lingyu-Meng <147720003+Lingyu-Meng@users.noreply.github.com>
2024-07-25 22:36:46 +0100

    feat{learning}: model-free for first 6 or last 6 trials

--- step4_robust_check/check3_learning_effect/s1_futher_explore.R
+++ step4_robust_check/check3_learning_effect/s1_futher_explore.R
@@ -72,7 +72,180 @@ rt_plot <- rt_data %>%
   geom_signif(comparisons = list(c("First 6", "Last 6")), map_signif_level = TRUE) +
   theme_cowplot()
 
-trial_effect <- cowplot::plot_grid(acc_plot, rt_plot, nrow = 1)
+trial_effect <- plot_grid(acc_plot, rt_plot, nrow = 1)
+
+# Whether the model free analysis result remains for the first 6 trials?
+# Accuracy
+data_f6 <- data %>% 
+  filter(trial <= 6)
+
+acc_glmm_f6 <- glmer(`Store: trial_correct` ~  context * arms + (1|`Participant Private ID`),
+    data = data_f6, family = "binomial")
+summary(acc_glmm_f6)
+acc_posthoc_f6 <- lsmeans(acc_glmm_f6, pairwise ~ context*arms, adjust = "tukey") # post hoc test
+
+# Visiualise the HLM results
+# Create a data context for visualization
+relogit <- function(x) {
+  exp(x)/(1+exp(x)) # reverse logit
+}
+vis_data_acc_f6 <- acc_posthoc_f6$lsmeans %>% 
+  as.data.frame() %>%
+  mutate(performance = relogit(lsmean),
+         performance_LCI = relogit(asymp.LCL),
+         performance_UCI = relogit(asymp.UCL)) 
+
+# Plot the interaction
+acc_cond_inter_f6 <- vis_data_acc_f6 %>% 
+  ggplot(aes(x = context, y = performance, color = arms, group = arms)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3, position=position_dodge(0.05)) +
+  geom_errorbar(aes(ymin = performance_LCI, ymax = performance_UCI), width = 0.05,
+                position=position_dodge(0.05)) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("NS."), tip_length = 0,
+              y_position = 0.57) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("*"), tip_length = 0,
+              y_position = 0.568, colour = "#00BFC4",
+              vjust = 2.3) +
+  geom_signif(y_position = 0.566, xmin = 0.95, xmax = 1.05, 
+              annotation = c("***"), tip_length = 0, 
+              colour = "black") +
+  geom_signif(y_position = 0.566, xmin = 1.95, xmax = 2.05, 
+              annotation = c("**"), tip_length = 0, 
+              colour = "black") +
+  labs(color = "Arms",
+       y = "Performance ",
+       x = "Context"
+  ) +
+  theme_cowplot()
+
+# RT
+glmm_RT_f6 <- glmer(`Reaction Time` ~ context*arms + (1|`Participant Private ID`),
+                    data = data_f6, family = Gamma(link = "log")) # Gamma distribution
+summary(glmm_RT_f6) # WIN SR as baseline
+lnRT_posthoc_f6 <- lsmeans(glmm_RT_f6, pairwise ~ context:arms, adjust = "tukey") # post hoc test
+
+# Visiualise the HLM results
+vis_data_lnRT_f6 <- lnRT_posthoc_f6$lsmeans %>% 
+  as.data.frame() %>%
+  mutate(lnRT = lsmean,
+         lnRT_LCI = asymp.LCL,
+         lnRT_UCI = asymp.UCL)
+
+# Plot the interaction
+lnRT_cond_inter_f6 <- vis_data_lnRT_f6 %>% 
+  ggplot(aes(x = context, y = lnRT, color = arms, group = arms)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3, position=position_dodge(0.05)) +
+  geom_errorbar(aes(ymin = lnRT_LCI, ymax = lnRT_UCI), width = 0.05,
+                position=position_dodge(0.05)) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              y_position = 6.745,
+              annotation = c("***"), tip_length = 0) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("**"), tip_length = 0,
+              y_position = 6.743,
+              vjust = 2.3, colour = "#00BFC4") +
+  geom_signif(y_position = 6.74, xmin = 0.95, xmax = 1.05,
+              annotation = c("NS."), tip_length = 0,
+              colour = "black") +
+  geom_signif(y_position = 6.74, xmin = 1.95, xmax = 2.05,
+              annotation = c("NS."), tip_length = 0,
+              colour = "black") +
+  labs(color = "Arms",
+       y = "Log RT",
+       x = "Context") +
+  theme_cowplot()
+
+first_6 <- plot_grid(acc_cond_inter_f6, lnRT_cond_inter_f6, nrow = 1)
+
+# Whether the model free analysis result remains for the last 6 trials?
+# Accuracy
+data_l6 <- data %>% 
+  filter(trial > 6)
+
+acc_glmm_l6 <- glmer(`Store: trial_correct` ~  context * arms + (1|`Participant Private ID`),
+    data = data_l6, family = "binomial")
+summary(acc_glmm_l6)
+acc_posthoc_l6 <- lsmeans(acc_glmm_l6, pairwise ~ context*arms, adjust = "tukey") # post hoc test
+
+# Visiualise the HLM results
+# Create a data context for visualization
+vis_data_acc_l6 <- acc_posthoc_l6$lsmeans %>% 
+  as.data.frame() %>%
+  mutate(performance = relogit(lsmean),
+         performance_LCI = relogit(asymp.LCL),
+         performance_UCI = relogit(asymp.UCL))
+
+# Plot the interaction
+acc_cond_inter_l6 <- vis_data_acc_l6 %>% 
+  ggplot(aes(x = context, y = performance, color = arms, group = arms)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3, position=position_dodge(0.05)) +
+  geom_errorbar(aes(ymin = performance_LCI, ymax = performance_UCI), width = 0.05,
+                position=position_dodge(0.05)) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("NS."), tip_length = 0,
+              y_position = 0.665) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("NS."), tip_length = 0,
+              y_position = 0.663, colour = "#00BFC4",
+              vjust = 2.3) +
+  geom_signif(y_position = 0.66, xmin = 0.95, xmax = 1.05, 
+              annotation = c("***"), tip_length = 0, 
+              colour = "black") +
+  geom_signif(y_position = 0.66, xmin = 1.95, xmax = 2.05, 
+              annotation = c("***"), tip_length = 0, 
+              colour = "black") +
+  labs(color = "Arms",
+       y = "Performance ",
+       x = "Context"
+  ) +
+  theme_cowplot()
+
+# RT
+glmm_RT_l6 <- glmer(`Reaction Time` ~ context*arms + (1|`Participant Private ID`),
+                    data = data_l6, family = Gamma(link = "log")) # Gamma distribution
+summary(glmm_RT_l6) # WIN SR as baseline
+lnRT_posthoc_l6 <- lsmeans(glmm_RT_l6, pairwise ~ context:arms, adjust = "tukey") # post hoc test
+
+# Visiualise the HLM results
+vis_data_lnRT_l6 <- lnRT_posthoc_l6$lsmeans %>% 
+  as.data.frame() %>%
+  mutate(lnRT = lsmean,
+         lnRT_LCI = asymp.LCL,
+         lnRT_UCI = asymp.UCL)
+
+# Plot the interaction
+lnRT_cond_inter_l6 <- vis_data_lnRT_l6 %>% 
+  ggplot(aes(x = context, y = lnRT, color = arms, group = arms)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3, position=position_dodge(0.05)) +
+  geom_errorbar(aes(ymin = lnRT_LCI, ymax = lnRT_UCI), width = 0.05,
+                position=position_dodge(0.05)) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              y_position = 6.57,
+              annotation = c("***"), tip_length = 0) +
+  geom_signif(comparisons = list(c("Win", "Lose")),
+              annotation = c("**"), tip_length = 0,
+              y_position = 6.567,
+              vjust = 2.3, colour = "#00BFC4") +
+  geom_signif(y_position = 6.56, xmin = 0.95, xmax = 1.05,
+              annotation = c("NS."), tip_length = 0,
+              colour = "black") +
+  geom_signif(y_position = 6.56, xmin = 1.95, xmax = 2.05,
+              annotation = c("NS."), tip_length = 0,
+              colour = "black") +
+  labs(color = "Arms",
+       y = "Log RT",
+       x = "Context") +
+  theme_cowplot()
+
+last_6 <- plot_grid(acc_cond_inter_l6, lnRT_cond_inter_l6, nrow = 1)
 
 # Save the plot
-ggsave("step2_descriptive_statistics/output/trial_effect.png", plot = trial_effect, width = 10, height = 5)
+ggsave("step4_robust_check/check3_learning_effect/output/trial_effect.png", plot = trial_effect, width = 10, height = 5)
+ggsave("step4_robust_check/check3_learning_effect/output/first_6.png", plot = first_6, width = 10, height = 5)
+ggsave("step4_robust_check/check3_learning_effect/output/last_6.png", plot = last_6, width = 10, height = 5)
