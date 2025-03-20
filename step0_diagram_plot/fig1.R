fa726522946fb5988d9e33bee0d1af19db1baaf3
Lingyu-Meng <147720003+Lingyu-Meng@users.noreply.github.com>
2025-03-20 11:09:05 +0000

    update{fig1}: save panel separately

--- step0_diagram_plot/fig1.R
+++ step0_diagram_plot/fig1.R
@@ -23,10 +23,12 @@ r <- c(1, 1, 1, 2, 2, 2, 2, 2, 2, 3, 3, 3)
 S <- c(5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5)
 
 ## plot the probability distribution of the reward
-df <- data.frame(R = R, r = r, S = S) %>%
+df_rR <- data.frame(R = R, r = r) %>%
+  gather(key = "condition", value = "reward")
+df_SR <- data.frame(R = R, S = S) %>%
   gather(key = "condition", value = "reward")
 
-panel_B <- df %>%
+panel_B <- df_rR %>%
   ggplot(aes(x = reward, fill = condition)) +
   geom_histogram(binwidth = 1, position = "identity", alpha = 0.5) +
 ### add kerbal density estimation for R and r
@@ -34,21 +36,39 @@ panel_B <- df %>%
                aes(y = ..count..), alpha = 0.3,
                adjust = 3) +
   labs(x = "Reward", y = "Frequency", fill = "Arm") +
-  scale_fill_manual(values = c("red", "blue", "green")) +
+  scale_fill_manual(values = c("green", "red")) +
   theme_cowplot() +
   scale_x_continuous(breaks = seq(1, 7, 1)) +
-  scale_y_continuous(breaks = seq(0, 12, 2)) +
+  scale_y_continuous(breaks = seq(0, 12, 3)) +
+  theme(legend.position = "top") +
+  ylim(0, 11.5) # it is weird that the ylim should be 11.5 to make it as 12
+
+panel_C <- df_SR %>%
+  ggplot(aes(x = reward, fill = condition)) +
+  geom_histogram(binwidth = 1, position = "identity", alpha = 0.5) +
+### add kerbal density estimation for R
+  geom_density(data = df %>% filter(condition %in% "R"),
+               aes(y = ..count..), alpha = 0.3,
+               adjust = 3) +
+  labs(x = "Reward", y = "Frequency", fill = "Arm") +
+  scale_fill_manual(values = c("red", "blue")) +
+  theme_cowplot() +
+  scale_x_continuous(breaks = seq(1, 7, 1)) +
+  scale_y_continuous(breaks = seq(0, 12, 3)) +
   theme(legend.position = "top")
 
-ggsave(file.path(save_path, "fig1_panel_B.png"), panel_B, width = 7, height = 7)
+ggsave(file.path(save_path, "fig1_panel_B.png"), panel_B, width = 5, height = 5)
+ggsave(file.path(save_path, "fig1_panel_C.png"), panel_C, width = 5, height = 5)
 
-# Panel C, psychometric curve for directed exploration, uncertainty-dependent random exploration, and uncertainty-independent random exploration
+# Panel D, psychometric curve for directed exploration, uncertainty-dependent random exploration, and uncertainty-independent random exploration
 ## Directed exploration
 V <- seq(-6, 6, 0.01)
 no_dir   <- pnorm(V + 0 + V/ 1.72) # 1.72 is the standard deviation of the population (TU)
 more_dir <- pnorm(V + 1 + V/ 1.72)
-data_dir <- data.frame(V, no_dir, more_dir) %>% 
-  gather(key = "condition", value = "P", -V)
+negate_dir <- pnorm(V - 1 + V/ 1.72)
+data_dir <- data.frame(V, more_dir, no_dir, negate_dir) %>% 
+  gather(key = "condition", value = "P", -V) %>% 
+  mutate(condition = factor(condition, levels = c("more_dir", "no_dir", "negate_dir")))
 
 SR <- pnorm(V + 0.5 + V/ 1.72)
 rR <- pnorm(V + 0.5 * 0.55 + V/ 1.89) # 1.89 is the standard deviation of the population (TU)
@@ -59,13 +79,14 @@ data_SR_rR <- data.frame(V, SR, rR) %>%
 directed_ex <- ggplot(data_dir, aes(x = V, y = P, linetype = condition)) +
   geom_line(linewidth = 2, alpha = 0.5) +
   labs(x = "Expected value difference V(1) - V(2)",
-       y = "Choice Probability P(1)", linetype = "Directed exploration") +
-  scale_linetype_manual(values = c("solid", "dotdash"),
-                        labels = c("High", "No")) +
+       y = "Choice Probability P(1)", linetype = "DE") +
+  scale_linetype_manual(values = c("solid", "dotdash", "dotted"),
+                        labels = c("Risk Seeking", "Risk neutral", "Risk Averse")) +
   theme_cowplot() +
   scale_x_continuous(breaks = seq(-6, 6, 2)) +
   scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-  theme(legend.position = "top")
+  theme(legend.position = "top") +
+  theme(legend.key.width = unit(1.5,"cm"))
 
 ggsave(file.path(save_path, "fig1_panel_C_directed_exploration.png"), directed_ex, width = 7, height = 7)
 
@@ -84,13 +105,13 @@ UDR_ex <- ggplot(data_UDR, aes(x = V, y = P, linetype = UDR, colour = uncertaint
   geom_line(linewidth = 2, alpha = 0.5) +
   labs(x = "Expected value difference V(1) - V(2)",
        y = "Choice Probability P(1)",
-       linetype = "Uncertainty-dependent random exploration",
+       linetype = "UDRE",
        colour = "Uncertainty") +
   scale_linetype_manual(values = c("solid", "dotdash")) +
   theme_cowplot() +
   scale_x_continuous(breaks = seq(-6, 6, 2)) +
   scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-  theme(legend.position = "top", legend.box="vertical")
+  theme(legend.position = "top")
 
 ggsave(file.path(save_path, "fig1_panel_C_UDR_exploration.png"), UDR_ex, width = 7, height = 7)
 
@@ -109,13 +130,13 @@ UIR_ex <- ggplot(data_UIR, aes(x = V, y = P, linetype = UIR, colour = uncertaint
   geom_line(linewidth = 2, alpha = 0.5) +
   labs(x = "Expected value difference V(1) - V(2)",
        y = "Choice Probability P(1)",
-       linetype = "Uncertainty-independent random exploration",
+       linetype = "UIRE",
        colour = "Uncertainty") +
   scale_linetype_manual(values = c("solid", "dotdash")) +
   theme_cowplot() +
   scale_x_continuous(breaks = seq(-6, 6, 2)) +
   scale_y_continuous(breaks = seq(0, 1, 0.2)) +
-  theme(legend.position = "top", legend.box="vertical")
+  theme(legend.position = "top")
 
 ggsave(file.path(save_path, "fig1_panel_C_UIR_exploration.png"), UIR_ex, width = 7, height = 7)
 
